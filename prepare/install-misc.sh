40348c29b9593889298e75a16438ef3b9235a9e2
Ben Li <bash99@gmail.com>
2019-08-06 06:23:16 +0000

    fix for some bug image without epel

--- prepare/install-misc.sh
+++ prepare/install-misc.sh
@@ -1,5 +1,9 @@
-yum install epel-release -y \
+GNUPGHOME=/etc/gnupg \
+gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 352c64e5 \
+&& gpg --export --armor 352c64e5 > ${GNUPGHOME}/RPM-GPG-KEY-EPEL
+&& yum install epel-release -y \
 && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A \
 && gpg --export --armor 430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A > ${GNUPGHOME}/RPM-GPG-KEY-Percona \
+&& rpm --import ${GNUPGHOME}/RPM-GPG-KEY-*
 && yum -y install -y jemalloc pxz numactl-devel rh-mysql57-mecab-devel bzip2 cmake libaio-devel \
       ncurses-devel readline-devel libcurl-devel pam-devel bison-devel bison tmux bc patch
