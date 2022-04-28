d50b50306aed112ffa19a46caf23d1cf9cc91e80
Ben Li <bash99@gmail.com>
2022-04-28 03:25:52 +0000

    update gpg keyserver as sks is offline

--- prepare/install-misc.sh
+++ prepare/install-misc.sh
@@ -2,10 +2,9 @@
 
 yum_install() {
 GNUPGHOME=~/gnupg && mkdir -p ~/gnupg && chmod 600 ~/gnupg \
-&& gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 352c64e5 \
+&& gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A 99DB70FAE1D7CE227FB6488205B555B38483C65D 352c64e5 \
 && gpg --export --armor 352c64e5 > ${GNUPGHOME}/RPM-GPG-KEY-EPEL \
 && yum install epel-release -y \
-&& gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A \
 && gpg --export --armor 430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A > ${GNUPGHOME}/RPM-GPG-KEY-Percona \
 && rpm --import ${GNUPGHOME}/RPM-GPG-KEY-* \
 && yum -y install -y jemalloc pxz numactl numactl-devel rh-mysql57-mecab-devel bzip2 cmake3 libaio-devel \
